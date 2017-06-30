4564b9c1e0b785504614981687c697a8689dc06b
casio <carsten.kraus@gmail.com>
2017-06-30 09:49:38 +0200

    asnc

--- src/stories/async.js
+++ src/stories/async.js
@@ -29,3 +29,34 @@ stories.add('async', _ => {
   
   return <div>Check console output</div>
 })
+
+stories.add('async in react component', _ => {
+  class Comp extends Component {
+    state = { user: 'n/a' }
+
+    fetchData () {
+      return fetch('https://randomuser.me/api/')
+    }
+    
+    async componentDidMount() {
+      try {
+        const response = await this.fetchData(),
+              {info, results: [user, ...rest]} = await response.json()
+        // NOTE: there will be a warning about unmounted component & setState
+        // @see https://medium.com/front-end-hacking/async-await-with-react-lifecycle-methods-802e7760d802
+        this.setState({user})
+      } catch(err) {
+        console.error(err)
+      }
+    }
+
+    render () {
+      const {user} = this.state
+      return <div>
+        A user: 
+        <pre>{JSON.stringify(user)}</pre>
+      </div>
+    }
+  }
+  return <Comp />
+})
